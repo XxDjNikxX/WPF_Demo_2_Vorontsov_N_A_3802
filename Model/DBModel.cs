aa19d5aa18284ba66e94946d0db94d53677ed139
DjNik <nikitos1235@gmail.com>
2021-09-07 19:12:05 +0300

    Added pictures, model, starting to desgin Pages

---
+++ Model/DBModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Demo_1_Vorontsov_N_A_3802
+{
+    static class DBModel
+    {
+        public static WorldSkills_First_DemoEntities DB = new WorldSkills_First_DemoEntities();
+    }
+}
---
+++ Pages/PageShowAgents.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Demo_1_Vorontsov_N_A_3802.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageShowAgents.xaml
+    /// </summary>
+    public partial class PageShowAgents : Page
+    {
+        public PageShowAgents()
+        {
+            InitializeComponent();
+        }
+    }
+}
