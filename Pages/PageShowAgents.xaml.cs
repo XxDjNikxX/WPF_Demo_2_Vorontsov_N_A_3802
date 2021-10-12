3dc69d06f52fd69d8f888d6b7ef64318edac2039
DjNik <nikitos1235@gmail.com>
2021-10-13 01:27:05 +0300

    Added Sorting

--- Pages/PageShowAgents.xaml.cs
+++ Pages/PageShowAgents.xaml.cs
@@ -32,8 +32,21 @@ namespace WPF_Demo_1_Vorontsov_N_A_3802.Pages
         /// <param name="e"></param>
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            
-             lbAgents.ItemsSource = DBModel.DB.Agent.ToList(); 
+
+            lbAgents.ItemsSource = DBModel.DB.Agent.ToList();
+
+            List<string> NamesToComboSorting = new List<string> { "Сортировка:", "От А до Я", "От Я до А" };
+
+            foreach (var a in NamesToComboSorting)
+            {
+                cbSort.Items.Add(a).ToString();
+            }
+
+            cbFilter.Items.Add("Группировка:");
+            foreach (var a in DBModel.DB.AgentType)
+            {
+                cbFilter.Items.Add(a).ToString();
+            }
 
         }
 
@@ -45,6 +58,64 @@ namespace WPF_Demo_1_Vorontsov_N_A_3802.Pages
         private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
             lbAgents.ItemsSource = DBModel.DB.Agent.Where(x => x.Title.StartsWith(tbSearch.Text)).ToList(); 
+        }
+
+        /// <summary>
+        /// Метод реализации сортировки данных
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (cbSort.SelectedIndex)
+            {
+                default:
+                    lbAgents.ItemsSource = DBModel.DB.Agent.ToList();
+                    break;
+                case 0:
+                    lbAgents.ItemsSource = DBModel.DB.Agent.ToList();
+                    break;
+                case 1: 
+                    lbAgents.ItemsSource = DBModel.DB.Agent.OrderBy(u => u.Title).ToList();
+                    break;
+                case 2:
+                    lbAgents.ItemsSource = DBModel.DB.Agent.OrderByDescending(u => u.Title).ToList();
+                    break;
+            }
+        }
+
+       /// <summary>
+       /// Алгоритм реализации группировки данных
+       /// </summary>
+       /// <param name="sender"></param>
+       /// <param name="e"></param>
+       private void cbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            switch (cbFilter.SelectedIndex)
+            {
+                default:
+                    lbAgents.ItemsSource = DBModel.DB.Agent.ToList();
+                    break;
+                case 0:
+                    lbAgents.ItemsSource = DBModel.DB.Agent.ToList();
+                    break;
+                case 1:
+                    lbAgents.ItemsSource = DBModel.DB.Agent.Where(u => u.AgentType.Title == "МКК").ToList();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+            }
         }
     }
 }
